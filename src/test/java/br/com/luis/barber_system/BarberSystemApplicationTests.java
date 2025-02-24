b3b9f11116564aa6685807b141addbd806ee5547
Luis Henrique <luishenrique2859@hotmail.com>
2025-02-24 14:36:11 -0300

    initial config

---
+++ src/test/java/br/com/luis/barber_system/BarberSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luis.barber_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
